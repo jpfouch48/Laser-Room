e3cf3b0e9b696d5ecd22f9084557de35f0a1e0a4
Faucher <jon-paul.faucher@ad.dot.gov>
2020-10-05 06:43:15 -0400

    Initial Checkin

---
+++ Configuration.h
@@ -0,0 +1,66 @@
+#ifndef CONFIGURATION_HEADER
+#define CONFIGURATION_HEADER
+
+//#define DO_NOT_CONNECT
+
+// ****************************************************************************
+// WIFI Defines
+//  WIFI_SSID     - SSID of wireless network you want to connect to
+//  WIFI_SSID_PW  - Password of wireless network  
+// ****************************************************************************
+#define WIFI_SSID                 "river-house"
+#define WIFI_SSID_PW              "katiejpfinnegan1"
+
+// ****************************************************************************
+// MQTT Defines
+// ****************************************************************************
+#define MQTT_SERVER_IP              "192.168.1.3"
+#define MQTT_SERVER_PORT            1883
+#define MQTT_USER                   "***REMOVED***"
+#define MQTT_USER_PW                "***REMOVED***"
+
+#define MQTT_SENSOR_NAME            "MQTT_LASER_UTIL_001"
+
+// MQTT Base Topics
+#define MQTT_BASE_TOPIC             "sensor/" MQTT_SENSOR_NAME "/laser/"
+#define MQTT_ALL_TOPICS             MQTT_BASE_TOPIC "#"
+
+// MQTT Room Temp Topics
+#define MQTT_TOPIC_ROOM_TEMP        MQTT_BASE_TOPIC "room_temperature" 
+#define MQTT_TOPIC_ROOM_HUMIDITY    MQTT_BASE_TOPIC "room_humidity" 
+#define MQTT_TOPIC_ROOM_DEWPOINT    MQTT_BASE_TOPIC "room_dewpoint" 
+
+// MQTT Room Temp Topics
+#define MQTT_TOPIC_CHILLER_TEMP     MQTT_BASE_TOPIC "chiller_temp"
+
+// MQTT RGB Topics
+#define MQTT_TOP_RGB_POWER          MQTT_BASE_TOPIC "rgb_power"
+#define MQTT_TOP_RGB_COLORR         MQTT_BASE_TOPIC "rgb_colorr"
+#define MQTT_TOP_RGB_COLORG         MQTT_BASE_TOPIC "rgb_colorg"
+#define MQTT_TOP_RGB_COLORB         MQTT_BASE_TOPIC "rgb_colorb"
+#define MQTT_TOP_RGB_BRIGHTNESS     MQTT_BASE_TOPIC "rgb_brightness"
+
+// ****************************************************************************
+// OTA Defines
+// ****************************************************************************
+#define OTA_PW                      ""
+#define OTA_PORT                    8266
+
+// ****************************************************************************
+// PIN Definitions
+//   - DHT_PIN     - Room Temp Sensor pin (temp/humidity)
+//   - RGB_PIN     - RGB LED Control pin
+//   - DS18B20_PIN - Chiller Temp Sensor pin
+// ****************************************************************************
+#define DHT_PIN                     5
+#define RGB_PIN                     14
+#define DS18B20_PIN                 0
+ 
+// ****************************************************************************
+// RGB Definitions
+// ****************************************************************************
+#define RGB_LED_TYPE                WS2812B
+#define RGB_COLOR_ORDER             GRB
+#define RGB_NUM_LEDS                20
+
+#endif // CONFIGURATION_HEADER
---
+++ Laser-Room.ino
@@ -0,0 +1,253 @@
+// General Includes
+#include <stdlib.h>
+
+// WIFI/MQTT Includes
+#include <ESP8266WiFi.h>
+#include <PubSubClient.h>
+
+// Local Includes
+#include "Configuration.h"
+#include "TempWrapper.h"
+#include "FastLedWrapper.h"
+
+
+#include <OneWire.h> 
+#include <DallasTemperature.h>
+
+// Temp Wrapper
+TempWrapper gTempWrapper(DHT_PIN);
+OneWire gOneWire(DS18B20_PIN); 
+DallasTemperature gChillerSensors(&gOneWire);
+
+// Connection Wrappers
+WiFiClient gWifiClient;
+PubSubClient gMqttClient(gWifiClient);
+
+// Used for string manipulations and buffer management
+#define SZ_BUFFER_WIDTH 128
+char gSzBuffer[SZ_BUFFER_WIDTH];
+
+#define MQTT_BUFFER_WIDTH 256
+char gMqttBuffer[MQTT_BUFFER_WIDTH];
+
+// RGB Defines
+FastLedWrapper gRgbWrapper;
+
+enum AppState
+{
+  AppState_WifiConnecting,
+  AppState_MqttConnecting,
+  AppState_Running
+};
+AppState gAppState;
+
+void change_state(AppState aState)
+{
+  gAppState = aState;
+
+  if(gAppState == AppState::AppState_WifiConnecting)
+  {
+    gRgbWrapper.set_color(CRGB(255, 0, 0));
+    Serial.println("Changing to WifiConnecting state");
+  }
+  else if(gAppState == AppState::AppState_MqttConnecting)
+  {
+    gRgbWrapper.set_color(CRGB(0, 255, 0));
+    Serial.println("Changing to MqttConnecting state");
+  }
+  else if(gAppState == AppState::AppState_Running)
+  {
+    gRgbWrapper.set_color(CRGB(255, 255, 255));
+    Serial.println("Changing to Running state");    
+  }
+}
+
+// ****************************************************************************
+//
+// ****************************************************************************
+void setup()
+{  
+  Serial.begin(9600);
+
+  // Setup Temp Sensor
+  gTempWrapper.setup();
+  gChillerSensors.begin();
+
+  // Setup RGB
+  gRgbWrapper.setup();
+  gRgbWrapper.set_color(CRGB(255, 0, 0));
+  gRgbWrapper.set_brightness(50);
+  gRgbWrapper.set_pattern(FastLedWrapper::PatternType::Pattern_Cylon);
+
+#ifndef DO_NOT_CONNECT  
+  delay(10);
+  // We start by connecting to a WiFi network
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(WIFI_SSID);
+
+  WiFi.begin(WIFI_SSID, WIFI_SSID_PW);
+
+  change_state(AppState::AppState_WifiConnecting);
+#else
+  change_state(AppState::AppState_Running);
+#endif
+}
+
+// ****************************************************************************
+//
+// ****************************************************************************
+void loop()
+{
+  if(gAppState == AppState::AppState_WifiConnecting)
+  {
+    EVERY_N_MILLISECONDS(5000)     
+    {
+      if (WiFi.status() == WL_CONNECTED) 
+      {
+        Serial.println("");
+        Serial.println("WiFi connected");
+        Serial.println("IP address: ");
+        Serial.println(WiFi.localIP());
+
+        gMqttClient.setServer(MQTT_SERVER_IP, MQTT_SERVER_PORT);  
+        gMqttClient.setCallback(mqtt_callback);  
+
+        change_state(AppState::AppState_MqttConnecting);
+      }
+      else
+      {
+        Serial.println("Connecting to WIFI");
+      }
+    }
+  }
+  else if(gAppState == AppState::AppState_MqttConnecting)
+  {
+    EVERY_N_MILLISECONDS(5000)     
+    {
+      if (!gMqttClient.connected()) 
+      {
+        Serial.println("Connecting to MQTT");
+
+        if (gMqttClient.connect(MQTT_SENSOR_NAME, MQTT_USER, MQTT_USER_PW)) 
+        {
+          Serial.println("Connected to MQTT");
+          change_state(AppState::AppState_Running);
+        }
+        else 
+        {
+          Serial.print("failed, rc=");
+          Serial.print(gMqttClient.state());
+          Serial.println(" try again in 5 seconds");
+        }
+      }
+      else
+      {
+         change_state(AppState::AppState_Running);
+      }      
+    }
+  }
+  else if(gAppState == AppState_Running)
+  {
+    // Connection processing
+#ifndef DO_NOT_CONNECT  
+    if (!gMqttClient.connected()) 
+    {
+      change_state(AppState::AppState_MqttConnecting);
+    }  
+    else
+#endif  
+    {
+#ifndef DO_NOT_CONNECT  
+      gMqttClient.loop();
+#endif  
+
+      EVERY_N_MILLISECONDS(5000)     
+      {
+        // Chiller temp processing
+        gChillerSensors.requestTemperatures();
+#ifndef DO_NOT_CONNECT  
+        gMqttClient.publish(MQTT_TOPIC_CHILLER_TEMP, dtostrf(gChillerSensors.getTempCByIndex(0),      7, 3, gSzBuffer), true);    
+#endif
+        Serial.print("Chiller Temp: "); 
+        Serial.println(dtostrf(gChillerSensors.getTempFByIndex(0),      7, 3, gSzBuffer));
+
+        // Room Temp Processing
+        if(true == gTempWrapper.loop())
+        {
+#ifndef DO_NOT_CONNECT  
+          gMqttClient.publish(MQTT_TOPIC_ROOM_TEMP,     dtostrf(gTempWrapper.get_temp(),      7, 3, gSzBuffer), true);    
+          gMqttClient.publish(MQTT_TOPIC_ROOM_HUMIDITY, dtostrf(gTempWrapper.get_humidity(),  7, 3, gSzBuffer), true);    
+          gMqttClient.publish(MQTT_TOPIC_ROOM_DEWPOINT, dtostrf(gTempWrapper.get_dew_point(), 7, 3, gSzBuffer), true);    
+#endif
+
+          Serial.print("Temp: ");        Serial.print(dtostrf(gTempWrapper.get_temp(),      7, 3, gSzBuffer));
+          Serial.print(" Humidity: ");   Serial.print(dtostrf(gTempWrapper.get_humidity(),  7, 3, gSzBuffer));
+          Serial.print(" Dew Point: ");  Serial.print(dtostrf(gTempWrapper.get_dew_point(), 7, 3, gSzBuffer));
+          Serial.println();
+        }
+      }
+    }
+  }
+
+  // RGB Processing
+  gRgbWrapper.loop();
+}
+
+// ****************************************************************************
+//
+// ****************************************************************************
+void mqtt_callback(char* aTopic, byte* aPayload, unsigned int aLength) 
+{
+  Serial.print("mqtt_callback - ");
+  Serial.print("Topic: ");
+  Serial.println(aTopic);
+
+  if(aLength > MQTT_BUFFER_WIDTH)
+  {
+    // TODO: LOG AN ERROR - MESSAGE IS TO LARGE FOR BUFFER
+    Serial.println("Payload to large");
+  }
+  else
+  {
+    memcpy(gMqttBuffer, aPayload, aLength);
+    gMqttBuffer[aLength] = '\0';
+
+    Serial.print("  Payload: ");
+    Serial.println(gMqttBuffer);
+
+    if(0 == strcmp(aTopic, MQTT_TOP_RGB_POWER))
+    {
+      if(0 == strcmp(gMqttBuffer, "ON"))
+      {
+        gRgbWrapper.set_enabled(true);
+        gMqttClient.publish(aTopic, "ON");
+      }
+      else
+      {        
+        gRgbWrapper.set_enabled(false);
+        gMqttClient.publish(aTopic, "OFF");
+      }
+    }
+    else if(0 == strcmp(aTopic, MQTT_TOP_RGB_COLORR))
+    {
+      gRgbWrapper.set_color_red(atoi(gMqttBuffer));
+      gMqttClient.publish(aTopic, gMqttBuffer);      
+    }
+    else if(0 == strcmp(aTopic, MQTT_TOP_RGB_COLORG))
+    {
+      gRgbWrapper.set_color_green(atoi(gMqttBuffer));
+      gMqttClient.publish(aTopic, gMqttBuffer);      
+    }
+    else if(0 == strcmp(aTopic, MQTT_TOP_RGB_COLORB))
+    {
+      gRgbWrapper.set_color_blue(atoi(gMqttBuffer));
+      gMqttClient.publish(aTopic, gMqttBuffer);      
+    }
+    else if(0 == strcmp(aTopic, MQTT_TOP_RGB_BRIGHTNESS))
+    {
+      gRgbWrapper.set_brightness(atoi(gMqttBuffer));
+      gMqttClient.publish(aTopic, gMqttBuffer);      
+    }
+  }
+}
